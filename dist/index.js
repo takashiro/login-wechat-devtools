e0d9d0a35975c4b53b8f2940d7844ac751370c07
Kazuichi Takashiro <takashiro@qq.com>
2020-12-13 15:51:33 +0800

    Break the action if any error occurs

--- dist/index.js
+++ dist/index.js
@@ -3,9 +3,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const core = require("@actions/core");
 const Launcher_1 = require("./base/Launcher");
 (async function main() {
-    const launcher = new Launcher_1.default();
-    core.info('Preparing user data directory....');
-    await launcher.prepare();
-    core.info('Start login request. A QR Code will be send to your email address.');
-    await launcher.login();
+    try {
+        const launcher = new Launcher_1.default();
+        core.info('Preparing user data directory....');
+        await launcher.prepare();
+        core.info('Start login request. A QR Code will be send to your email address.');
+        await launcher.login();
+    }
+    catch (error) {
+        core.setFailed(error);
+    }
 }());
--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,15 @@ import * as core from '@actions/core';
 import Launcher from './base/Launcher';
 
 (async function main(): Promise<void> {
-	const launcher = new Launcher();
+	try {
+		const launcher = new Launcher();
 
-	core.info('Preparing user data directory....');
-	await launcher.prepare();
+		core.info('Preparing user data directory....');
+		await launcher.prepare();
 
-	core.info('Start login request. A QR Code will be send to your email address.');
-	await launcher.login();
+		core.info('Start login request. A QR Code will be send to your email address.');
+		await launcher.login();
+	} catch (error) {
+		core.setFailed(error);
+	}
 }());
